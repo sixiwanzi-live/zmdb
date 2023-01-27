da7078d7dcfdb158a9525bfdf1d564936af4d97f
sixiwanzi-live <sixiwanzi.live@outlook.com>
2023-01-27 22:55:47 +0800

    配置文件中segment的url改为tool的url

--- backend/api/SegmentApi.js
+++ backend/api/SegmentApi.js
@@ -3,7 +3,7 @@ import config from '../config.js';
 
 export default class SegmentApi {
     static async segment(clipId, startTime, endTime, audio) {
-        const url = `${config.segment.url}/clips/${clipId}/segments?startTime=${startTime}&endTime=${endTime}&audio=${audio}`;
+        const url = `${config.tool.url}/clips/${clipId}/segments?startTime=${startTime}&endTime=${endTime}&audio=${audio}`;
         const res = await fetch(url);
         return await res.json();
     }
